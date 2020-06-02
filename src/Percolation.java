203eaa19ca9fcd5bbf7df0c704035697ff2477bc
icycoke <reviveclsun@outlook.com>
2020-06-01 22:52:16 -0400

    Finish Percolation.java

---
+++ src/Percolation.java
@@ -0,0 +1,65 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    private WeightedQuickUnionUF uf;
+    private boolean[][] grid;
+    private int n;
+    private int count;
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.n = n;
+        count = 0;
+        uf = new WeightedQuickUnionUF(n * n + 2);
+        grid = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = false;
+            }
+        }
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        validate(row, col);
+        if (!isOpen(row, col)) {
+            grid[row - 1][col - 1] = true;
+            count++;
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        validate(row, col);
+        return grid[row - 1][col - 1];
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        validate(row, col);
+        if (!isOpen(row, col)) {
+            return false;
+        }
+        return uf.find((row - 1) * n + col) == uf.find(0);
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        return count;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return uf.find(0) == uf.find(n * n + 1);
+    }
+
+    private void validate(int row, int col) {
+        if (row < 1 || row > n || col < 1 || col > n) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
