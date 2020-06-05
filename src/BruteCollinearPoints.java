0166ef8b7130b018c6d7ffd96371d86fc4d205eb
icycoke <reviveclsun@outlook.com>
2020-06-05 19:29:52 -0400

    Optimize the BruteCollinearPoints.java

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -3,30 +3,30 @@ import java.util.List;
 
 public class BruteCollinearPoints {
 
-    private final Point[] points;
     private int numberOfSegments;
-    private List<LineSegment> segmentList;
+    private List<LineSegment> lineSegmentList;
 
     public BruteCollinearPoints(Point[] points) {
         if (points == null) {
             throw new IllegalArgumentException();
         }
-        segmentList = new ArrayList<>();
-        this.points = points;
-        numberOfSegments = 0;
-        if (points.length < 4)
+        if (points.length < 4) {
             return;
+        }
+        lineSegmentList = new ArrayList<>();
+        numberOfSegments = 0;
         for (int p = 0; p < points.length - 3; p++) {
             for (int q = p + 1; q < points.length - 2; q++) {
+                Point firstPoint = points[p];
+                double firstSlope = firstPoint.slopeTo(points[q]);
                 for (int r = q + 1; r < points.length - 1; r++) {
                     for (int s = r + 1; s < points.length; s++) {
-                        if (points[p] == null || points[q] == null || points[r] == null || points[s] == null)
+                        if (points[p] == null || points[q] == null || points[r] == null || points[s] == null) {
                             throw new IllegalArgumentException();
-                        Point firstPoint = points[p];
+                        }
                         Point endPoint1 = points[p];
                         Point endPoint2 = points[p];
                         int[] toBeCompared = new int[]{q, r, s};
-                        double firstSlope = firstPoint.slopeTo(points[q]);
                         boolean isInLine = true;
                         for (int index : toBeCompared) {
                             if (firstPoint.compareTo(points[index]) == 0)
@@ -40,7 +40,8 @@ public class BruteCollinearPoints {
                             }
                         }
                         if (isInLine) {
-                            segmentList.add(new LineSegment(endPoint1, endPoint2));
+                            lineSegmentList.add(new LineSegment(endPoint1, endPoint2));
+                            numberOfSegments++;
                         }
                     }
                 }
@@ -54,7 +55,7 @@ public class BruteCollinearPoints {
     }
 
     public LineSegment[] segments() {
-        return segmentList.toArray(new LineSegment[0]);
+        return lineSegmentList.toArray(new LineSegment[0]);
     }
 
 }
